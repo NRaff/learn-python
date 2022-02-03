9f78c79bb0073f7c88cadb5e104b2c9130cc97e2
nraff <nraff510@gmail.com>
2022-02-03 12:36:38 -0800

    add win algos to board class

--- oop_ttt/board.py
+++ oop_ttt/board.py
@@ -4,7 +4,6 @@ class Board:
       Constructs a new board object
       """
       self.grid = self.createBoard(size)
-      self.remaining_pos = self.getPositions()
   
   def createBoard(self, size):
     return [['_'] * size for i in range(size)]
@@ -15,20 +14,49 @@ class Board:
       print(s.join(row))
     print()
 
-  def getPositions(self):
-    positions = []
-    for row in range(len(self.grid)):
-      for col in range(len(self.grid)):
-        positions.append([row, col])
-    return positions
-  
-  def grid(self):
-    return self.grid
-
-  def remaining_pos(self):
-    return self.remaining_pos
-
   def placeMarker(self, pos, marker):
     row = pos[0]
     col = pos[1]
     self.grid[row][col] = marker
+
+  def win(self, marker):
+    return self.winHorizontal(marker) or self.winDiagonal(marker) or self.winVertical(marker)
+
+  def winHorizontal(self, marker):
+    for row in self.grid:
+      didWin = True
+      for mark in row:
+        if mark is not marker:
+          didWin = False
+          break
+      if didWin: 
+        return True
+    return False
+
+  def winVertical(self, marker):
+    for col in range(len(self.grid)):
+      didWin = True
+      for row in range(len(self.grid)):
+        if self.grid[row][col] is not marker:
+          didWin = False
+          break
+      if didWin:
+        return True
+    return False
+
+  def winDiagonal(self, marker):
+    return self.winDownRight(marker) or self.winDownLeft(marker)
+
+  def winDownRight(self, marker):
+    for i in range(len(self.grid)):
+      if self.grid[i][i] is not marker:
+        return False
+    return True
+
+  def winDownLeft(self, marker):
+    for i in range(len(self.grid)):
+      row = i
+      col = -(i + 1)
+      if self.grid[row][col] is not marker:
+        return False
+    return True
--- oop_ttt/game.py
+++ oop_ttt/game.py
@@ -1,4 +1,22 @@
 from board import Board
+import random
 
-new_board = Board(3)
-new_board.printBoard()
+class Game:
+  def __init__(self) -> None:
+      self.board = Board()
+      self.remaining_pos = self.getPositions()
+      self.current_player = 'O'
+
+  def getPositions(self):
+    positions = []
+    grid = self.board.grid
+    for row in range(len(grid)):
+      for col in range(len(grid)):
+        positions.append([row, col])
+    return positions
+
+  def switchPlayer(self):
+    self.current_player = 'X' if self.current_player is 'O' else 'O'
+
+game = Game()
+print(game.remaining_pos)
