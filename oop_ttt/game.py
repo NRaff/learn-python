195f32151ef571b1f3491c8ae10b76e23614e778
nraff <nraff510@gmail.com>
2022-02-03 12:47:02 -0800

    complete oop_ttt

--- oop_ttt/game.py
+++ oop_ttt/game.py
@@ -6,6 +6,7 @@ class Game:
       self.board = Board()
       self.remaining_pos = self.getPositions()
       self.current_player = 'O'
+      self.game_over = False
 
   def getPositions(self):
     positions = []
@@ -18,5 +19,25 @@ class Game:
   def switchPlayer(self):
     self.current_player = 'X' if self.current_player is 'O' else 'O'
 
+  def playTurn(self):
+    pos = random.sample(self.remaining_pos,1)[0]
+    self.board.placeMarker(pos, self.current_player)
+    self.board.printBoard()
+  
+  def newRound(self):
+    if self.board.win(self.current_player):
+      print(f'{self.current_player}\'s won!')
+      self.game_over = True
+    elif len(self.remaining_pos) == 0:
+      print(f'Game over - cat\'s game!')
+    else:
+      self.switchPlayer()
+
+  def playGame(self):
+    self.board.printBoard()
+    while not self.game_over:
+      self.playTurn()
+      self.newRound()
+
 game = Game()
-print(game.remaining_pos)
+game.playGame()
